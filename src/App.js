25dbc0054a427ce8d1d37013faa4ae378e7ef9ba
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-25 21:12:59 +0900

    05. props 를 통해 컴포넌트에게 값 전달하기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import Hello from "./Hello";
-import './App.css';
+import Wrapper from "./Wrapper";
 
 function App() {
-    const name = 'react';
-    const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: 24,
-        padding: '1rem'
-    };
     return (
-    <>
-        {/*주석*/}
-        <Hello
-        // 주석
-        />
-        <div style={style}>{name}</div>
-        <div className="gray-box"></div>
-    </>
+        <Wrapper>
+            <Hello name="react" color="red"/>
+            <Hello color="pink"/>
+        </Wrapper>
     );
 }
 
