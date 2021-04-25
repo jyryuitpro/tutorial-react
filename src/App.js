aa466d0d0136dcf9feeb1291f5e24533a9cb2101
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-25 14:23:23 +0900

    04. JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Hello from "./Hello";
+import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Hello/>
-    </div>
-  );
+    const name = 'react';
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24,
+        padding: '1rem'
+    };
+    return (
+    <>
+        {/*주석*/}
+        <Hello
+        // 주석
+        />
+        <div style={style}>{name}</div>
+        <div className="gray-box"></div>
+    </>
+    );
 }
 
 export default App;
