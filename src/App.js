32a54ca870de773148f26403202da940479bcc40
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-02 15:35:26 +0900

    12. useRef 로 useRef 로 컴포넌트 안의 변수 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import UserList from "./UserList";
 
 function App() {
+    const users = [
+        {
+            id: 1,
+            username: 'jyryuitpro',
+            email: 'jy.ryu.itpro@gmail.com',
+        },
+        {
+            id: 2,
+            username: 'test01',
+            email: 'test01@gmail.com',
+        },
+        {
+            id: 3,
+            username: 'test02',
+            email: 'test02@gmail.com',
+        },
+    ];
+
+    const nextId = useRef(4);
+
+    const onCreate = () => {
+        console.log(nextId.current);
+        nextId.current += 1;
+    };
+
     return (
-        <UserList/>
+        <UserList users={users}/>
     );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -8,25 +8,7 @@ function User({user}) {
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'jyryuitpro',
-            email: 'jy.ryu.itpro@gmail.com',
-        },
-        {
-            id: 2,
-            username: 'test01',
-            email: 'test01@gmail.com',
-        },
-        {
-            id: 3,
-            username: 'test02',
-            email: 'test02@gmail.com',
-        },
-    ];
-
+function UserList({users}) {
     return (
         <div>
             {
