eb9c92ae370ff1a27cbd6267e98a814dc9d479a6
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-02 16:14:43 +0900

    14. 배열에 항목 제거하기

--- src/App.js
+++ src/App.js
@@ -57,10 +57,14 @@ function App() {
         nextId.current += 1;
     };
 
+    const onRemove = (id) => {
+        setUsers(users.filter(user => user.id !== id));
+    };
+
     return (
         <>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-            <UserList users={users}/>
+            <UserList users={users} onRemove={onRemove}/>
         </>
     );
 }
