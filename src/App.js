ae0a5632261a3502923d373b69c7e9d9ec0e68b4
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-02 17:00:34 +0900

    15. 배열에 항목 수정하기

--- src/App.js
+++ src/App.js
@@ -23,16 +23,19 @@ function App() {
             id: 1,
             username: 'jyryuitpro',
             email: 'jy.ryu.itpro@gmail.com',
+            active: true,
         },
         {
             id: 2,
             username: 'test01',
             email: 'test01@gmail.com',
+            active: false,
         },
         {
             id: 3,
             username: 'test02',
             email: 'test02@gmail.com',
+            active: false,
         },
     ]);
 
@@ -61,10 +64,16 @@ function App() {
         setUsers(users.filter(user => user.id !== id));
     };
 
+    const onToggle = (id) => {
+        setUsers(users.map(
+            user => user.id === id ? {...user, active: !user.active} : user
+        ));
+    };
+
     return (
         <>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-            <UserList users={users} onRemove={onRemove}/>
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
         </>
     );
 }
